f9b12d7f1997f35cc6c5419a09aa269ff79646ea
Arzuaga <alberto.arzuaga@thecloud.group>
2024-02-15 12:24:33 -0500

    Agrega funcion de array de roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -25,7 +26,9 @@ class User extends Model
 
     protected $guarded = ['id'];
     protected $hidden = ['created_at', 'updated_at', 'password'];
+    protected $appends = ['rolesNames'];
 
+    protected $with = ['roles'];
 
     public function getActivitylogOptions(): LogOptions
     {
@@ -38,6 +41,14 @@ class User extends Model
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class, 'user_roles');
+
+    }
+
+    public function getrolesNamesAttribute()
+    {
+        $names = $this->roles()->pluck('name')->toArray();
+
+        return array_map(fn($value): string => Str::lower($value), $names);
 
     }
 
